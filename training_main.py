7142c168e6e5c57a2cba09e33522cb25a96e14f6
Arjun Gupta <argupta@mit.edu>
2018-11-05 15:39:31 -0500

    added ability to configure batch size from command_line

--- training_main.py
+++ training_main.py
@@ -23,11 +23,12 @@ if __name__ == '__main__':
     parser.add_argument('--load', '-l', type=str, nargs=1, help= 'A file location to load the model from',
                          dest = 'load_dir', default = '')
     parser.add_argument('--test', '-t', action = "store_true", help = "A flag to say that we are testing the model only")
-    parser.add_argument('--save-to', '-s', type = str, help = "A file location to store the model", dest = "save_dir")
+    parser.add_argument('--save-to', '-s', type = str, help = "A file location to store the model", dest = "save_dir", required=True)
     parser.add_argument('--log_iters', '-log', type = int, help = "The spacing between log printouts for training and testing", default = 7200)
     parser.add_argument('-lr', type = float, help = "the learning rate to use", default = .001)
     parser.add_argument('--cuda', '-c', action = "store_true", help = "Flag to use cuda for training and testing")
     parser.add_argument('--per_class', action="store_true", help="Flag to output per class data during training")
+    parser.add_argument('--batch_size', action= "store", help = "set the batch size for training and testing", default=1)
 
     args = parser.parse_args()
 
@@ -36,7 +37,7 @@ if __name__ == '__main__':
     epochs = DEFAULT_EPOCHS
     USE_CUDA = args.cuda
     DEFAULT_DEVICE = "cpu" if args.cuda else "cuda"
-    DEFAULT_BATCH = 1
+    DEFAULT_BATCH = args.batch_size
 
     img_path = "/home/arjun/MIT/6.867/project/bdd100k_images/bdd100k/images/100k"
     test_path = "/home/arjun/MIT/6.867/project/bdd100k_drivable_maps/bdd100k/drivable_maps/labels"
